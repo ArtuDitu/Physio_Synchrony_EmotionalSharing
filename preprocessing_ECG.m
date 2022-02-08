1e7e3e40c37673fe712e99f99fa8ac1a533e0ab9
Artur Czeszumski <aczeszumski@uos.de>
2022-02-08 11:39:36 +0100

    import data, create a filter, apply the filter

--- preprocessing_ECG.m
+++ preprocessing_ECG.m
@@ -3,4 +3,14 @@ cd D:\Dropbox\Projects\Emotional_Sharing_Physio
 addpath(genpath('D:\Dropbox\Projects\Emotional_Sharing_Physio'))
 
 %%
-d = importdata('SNS_005_11061518_ECG.txt');
+cd D:\Dropbox\Projects\Emotional_Sharing_Physio\Data\ECG 
+list_of_files = dir('**/*.txt'); % find all ECG files and create a list
+segments_info = readtable('segments_info.xlsx');
+
+
+
+d = importdata('SNS_005_11061518_ECG.txt'); % load data
+[b,a] = butter(5,[1 30]/250,'bandpass'); % create a filter 1-30 Hz, 500 Hz sampling rate, 5order
+d=filter(b,a,d.data(:,2));
+
+
