06f2fd71b6c5430a4f8236d1fb92e5c7b7ab442a
Artur Czeszumski <aczeszumski@uos.de>
2022-02-16 16:31:06 +0100

    add resting state 2,3 - add precision to calculation

--- ECG_analysis.m
+++ ECG_analysis.m
@@ -1,4 +1,6 @@
-%% change directory, add path
+%% change directory, add path\
+clear all
+clc
 cd D:\Dropbox\Projects\Emotional_Sharing_Physio
 addpath(genpath('D:\Dropbox\Projects\Emotional_Sharing_Physio'))
 
@@ -14,8 +16,8 @@ Rpeaks = cell(27,3,2);
 HR = zeros(27,3,2);
 
 RS1_size = [];
-sharing1_size = [];
-sharing2_size =[];
+emotional_size = [];
+neutral_size =[];
 
 % filter, epoch,, calculate peaks, and combine all data
 pairCount = 0;
@@ -31,47 +33,57 @@ for file = 1:size(list_of_files,1)
     d=filter(b,a,d.data(:,2)); % apply filter
     % epoch data
     d_RS1 = d(table2array(segments_info(file,4)):table2array(segments_info(file,5)));
+     d_RS2 = d(table2array(segments_info(file,8)):table2array(segments_info(file,9)));
+    d_RS3 = d(table2array(segments_info(file,12)):table2array(segments_info(file,13)));
     d_sharing1 = d(table2array(segments_info(file,6)):table2array(segments_info(file,7)));
-    d_sharing2 = d(table2array(segments_info(file,11)):table2array(segments_info(file,12)));
+    d_sharing2 = d(table2array(segments_info(file,10)):table2array(segments_info(file,11)));
     
+% remove noisy parts of data
     if file == 3
-        d_sharing2 = [d_sharing2(1:51050); d_sharing2(51890:end)];
+        d_sharing2 = d_sharing2(d_sharing2<2 & d_sharing2>-2);
+        d_RS3 = d_RS3(d_RS3<2 & d_RS3>-2);
     elseif file == 13
-        d_sharing2 = [d_sharing2(1:96988); d_sharing2(100900:128700); d_sharing2(129600:end)];
-    elseif  file == 29
-        d_RS1 = [d_RS1(1:121000); d_RS1(134500:end)];
+        d_sharing2 = d_sharing2(d_sharing2<1 & d_sharing2>-1);
+        d_RS2 = d_RS2(d_RS2<1 & d_RS2>-1);
+    elseif file == 29
+        d_RS1 = d_RS1(d_RS1<2.2 & d_RS1>-2.2);
     elseif file == 33
         d_sharing2 = d_sharing2(d_sharing2<2 & d_sharing2>-2);
-    elseif file == 36
+        d_RS3 = d_RS3(d_RS3<2 & d_RS3>-2);
+    elseif file ==36
         d_RS1 = d_RS1(d_RS1<1 & d_RS1>-1.5);
     elseif file == 37
         d_sharing2 = d_sharing2(d_sharing2<1.8);
+    elseif file == 40
+        d_RS3 = d_RS3(d_RS3<1.5 & d_RS3>-1.5);
     elseif file == 45
         d_sharing1 = d_sharing1(d_sharing1<1.5 & d_sharing1>-1.5);
-        d_sharing2 = d_sharing2(d_sharing2<2 & d_sharing2>-2);
+        d_sharing2 = d_sharing2(d_sharing2<1.5 & d_sharing2>-1.5);
+        d_RS3 = d_RS3(d_RS3<1.5 & d_RS3>-1.5);
     elseif file == 51
         d_RS1 = d_RS1(d_RS1<2 & d_RS1>-2);
-    elseif file == 53
-        d_sharing1 = d_sharing1(d_sharing1<1.5 & d_sharing1>-1.5);
-        d_sharing2 = d_sharing2(d_sharing2<1 & d_sharing2>-1.5);
+        d_sharing2 = d_sharing2(d_sharing2<2 & d_sharing2>-2);
+     elseif file == 53
+             d_sharing1 = d_sharing1(d_sharing1<1.5 & d_sharing1>-1.5);
+             d_sharing2 = d_sharing2(d_sharing2<1 & d_sharing2>-1.5);
+    elseif file ==7
+        d_RS3 = d_RS3(d_RS3<1 & d_RS3>-1.3);
     elseif file == 38
         d_sharing2 = d_sharing2(d_sharing2<1.5 & d_sharing2>-1.5);
     elseif file == 43
         d_sharing2 = d_sharing2(d_sharing2<1.5 & d_sharing2>-1.5);
+         d_RS2 = d_RS2(d_RS2<1.5 & d_RS2>-1.5);
     elseif file == 49
         d_sharing1 = d_sharing1(d_sharing1<1 & d_sharing1>-1);
     elseif file == 54
         d_sharing1 = d_sharing1(d_sharing1<1 & d_sharing1>-1);
-        d_sharing2 = d_sharing2(d_sharing2<1 & d_sharing2>-1);   
+             d_sharing2 = d_sharing2(d_sharing2<1 & d_sharing2>-1);
     end
-    
-    
-    
-    
+
     % find peaks
-    [pks_RS1, locs_RS1] = findpeaks(d_RS1,'MinPeakHeight',max(d_RS1)/2,'MinPeakDistance',125);
-    [pks_sharing1, locs_sharing1] = findpeaks(d_sharing1,'MinPeakHeight',max(d_sharing1)/2,'MinPeakDistance',125);
-    [pks_sharing2, locs_sharing2] = findpeaks(d_sharing2,'MinPeakHeight',max(d_sharing2)/2,'MinPeakDistance',125);
+    [pks_RS1, locs_RS1] = findpeaks(d_RS1,'MinPeakHeight',max(d_RS1)/2,'MinPeakDistance',250);
+    [pks_sharing1, locs_sharing1] = findpeaks(d_sharing1,'MinPeakHeight',max(d_sharing1)/2,'MinPeakDistance',250);
+    [pks_sharing2, locs_sharing2] = findpeaks(d_sharing2,'MinPeakHeight',max(d_sharing2)/2,'MinPeakDistance',250);
     
     %calc HR
     HR_RS1 = size(locs_RS1,1)/(size(d_RS1,1)/30000);
@@ -80,33 +92,41 @@ for file = 1:size(list_of_files,1)
     
     % combine all data
     if char(table2array(segments_info(file, 3))) == 'S'
-        Rpeaks(pairCount, 1, 1) = {locs_RS1'/500};
-        %HR(pairCount,1,1) = size(cell2mat(R(pairCount,1,1)),2)/(size(d_RS1,1)/30000);
+        Rpeaks(pairCount, 1, 1) = {locs_RS1'/(size(d_RS1,1)/300)};
+        HR(pairCount,1,1) = size(locs_RS1,1)/(size(d_RS1,1)/30000);
         if char(table2array(segments_info(file, 2))) == 'E'
-            Rpeaks(pairCount, 2, 1) = {locs_sharing1'/500};
-            Rpeaks(pairCount, 3, 1) = {locs_sharing2'/500};
-            %HR(pairCount,2,1) = size(cell2mat(R(pairCount,2,1)),2)/(size(d_sharing1,1)/30000);
-            %HR(pairCount,3,1) = size(cell2mat(R(pairCount,3,1)),2)/(size(d_sharing2,1)/30000);
+            emotional_size = [emotional_size size(d_sharing1,1)];
+            neutral_size =[neutral_size size(d_sharing2,1)];
+            Rpeaks(pairCount, 2, 1) = {locs_sharing1'/(size(d_sharing1,1)/300)};
+            Rpeaks(pairCount, 3, 1) = {locs_sharing2'/(size(d_sharing2,1)/300)};
+            HR(pairCount,2,1) = size(locs_sharing1,1)/(size(d_sharing1,1)/30000);
+            HR(pairCount,3,1) = size(locs_sharing2,1)/(size(d_sharing2,1)/30000);
         elseif char(table2array(segments_info(file, 2))) == 'N'
-            Rpeaks(pairCount, 2, 1) = {locs_sharing2'/500};
-            Rpeaks(pairCount, 3, 1) = {locs_sharing1'/500};
-            %HR(pairCount,2,1) = size(cell2mat(R(pairCount,2,1)),2)/(size(d_sharing2,1)/30000);
-            %HR(pairCount,3,1) = size(cell2mat(R(pairCount,3,1)),2)/(size(d_sharing1,1)/30000);
+            emotional_size = [emotional_size size(d_sharing2,1)];
+            neutral_size =[neutral_size size(d_sharing1,1)];
+            Rpeaks(pairCount, 2, 1) = {locs_sharing2'/(size(d_sharing1,1)/300)};
+            Rpeaks(pairCount, 3, 1) = {locs_sharing1'/(size(d_sharing2,1)/300)};
+            HR(pairCount,2,1) = size(locs_sharing2,1)/(size(d_sharing2,1)/30000);
+            HR(pairCount,3,1) = size(locs_sharing1,1)/(size(d_sharing1,1)/30000);
         end
         
     elseif char(table2array(segments_info(file, 3))) == 'L'
-        Rpeaks(pairCount, 1, 2) = {locs_RS1'/500};
-        %HR(pairCount,1,2) = size(cell2mat(R(pairCount,1,2)),2)/(size(d_RS1,1)/30000);
+        Rpeaks(pairCount, 1, 2) = {locs_RS1'/(size(d_RS1,1)/300)};
+        HR(pairCount,1,2) = size(locs_RS1,1)/(size(d_RS1,1)/30000);
         if char(table2array(segments_info(file, 2))) == 'E'
-            Rpeaks(pairCount, 2, 2) = {locs_sharing1'/500};
-            Rpeaks(pairCount, 3, 2) = {locs_sharing2'/500};
-            %HR(pairCount,2,2) = size(cell2mat(R(pairCount,2,2)),2)/(size(d_sharing1,1)/30000);
-            %HR(pairCount,3,2) = size(cell2mat(R(pairCount,3,2)),2)/(size(d_sharing2,1)/30000);
+            emotional_size = [emotional_size size(d_sharing1,1)];
+            neutral_size =[neutral_size size(d_sharing2,1)];
+            Rpeaks(pairCount, 2, 2) = {locs_sharing1'/(size(d_sharing1,1)/300)};
+            Rpeaks(pairCount, 3, 2) = {locs_sharing2'/(size(d_sharing2,1)/300)};
+            HR(pairCount,2,2) = size(locs_sharing1,1)/(size(d_sharing1,1)/30000);
+            HR(pairCount,3,2) = size(locs_sharing2,1)/(size(d_sharing2,1)/30000);
         elseif char(table2array(segments_info(file, 2))) == 'N'
-            Rpeaks(pairCount, 2, 2) = {locs_sharing2'/500};
-            Rpeaks(pairCount, 3, 2) = {locs_sharing1'/500};
-            %HR(pairCount,2,2) = size(cell2mat(R(pairCount,2,2)),2)/(size(d_sharing2,1)/30000);
-            %HR(pairCount,3,2) = size(cell2mat(R(pairCount,3,2)),2)/(size(d_sharing1,1)/30000);
+            emotional_size = [emotional_size size(d_sharing2,1)];
+            neutral_size =[neutral_size size(d_sharing1,1)];
+            Rpeaks(pairCount, 2, 2) = {locs_sharing2'/(size(d_sharing2,1)/300)};
+            Rpeaks(pairCount, 3, 2) = {locs_sharing1'/(size(d_sharing1,1)/300)};
+            HR(pairCount,2,2) = size(locs_sharing1,1)/(size(d_sharing2,1)/30000);
+            HR(pairCount,3,2) = size(locs_sharing2,1)/(size(d_sharing1,1)/30000);
         end
         
     end
@@ -123,7 +143,6 @@ neutral = Coincidence(:,:,3);
 neutral = neutral(EyeMat);
 
 [h,p,ci,stats] = ttest(emotional, neutral);
-
 
 
 
